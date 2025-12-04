be7bb2194647aa170d41e7b7790047bbab3e7383
Austin Cory Bart <acbart@udel.edu>
2025-12-04 14:54:10 -0500

    Add website title and site information setup

--- main.py
+++ main.py
@@ -3,8 +3,23 @@ from drafter import *
 from dataclasses import dataclass
 
 from meta import *
-from state import State
 
+# hide_debug_information()
+# set_website_framed(False)
+set_website_title("Your Drafter Website")
+set_site_information(
+    "author",
+    """
+Your description can go here.
+""",
+    [],
+    [],
+    [],
+)
+
+@dataclass
+class State:
+    pass
 
 @route
 def index(state: State) -> Page:
